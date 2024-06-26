395198dd01d9e01fbb088f6a7d69c3b73ea6ac16
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-06-26 20:13:53 +0530

    Sider

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,9 +1,13 @@
 export default function ProjectsSidebar(){
     return(
-        <aside>
-            <h2>Your Project</h2>
+        <aside className=" w-1/3 px-8 py-16 bg-stone-900 text-stone-100 md:w-72 rounded-r-xl  ">
+            <h2 className="md-8 uppercase font-bold text-stone-100 text-2xl pb-4 ">Your Project</h2>
+            <div className="my-6">
+                <button className="bg-stone-700 hover:bg-stone-600 text-stone-400 px-3 py-2 rounded-md">+ Add Project</button>
+            </div>
+            <ul>
 
-
+            </ul>
         </aside>
     );
 }
