a46810b534df6b5497dee09a1ae289e34b412122
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-06-30 22:21:20 +0530

    NEWTask upadate

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 export default function NewTask(){
+    const [enteredTask, setenteredTask] = useState();
+
+    function handleChange(event){
+        setenteredTask(event.target.value)
+    }
+
     return(
         <div className="flex items-center gap-4">
-            <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
+            <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" onChange={handleChange} value={enteredTask} />
             <button className="text-stone-700 hover:text-stone-950">Add Task</button>
         </div>
     );
